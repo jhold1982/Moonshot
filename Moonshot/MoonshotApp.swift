fb38825439b30959f3fed87e33702ad62119a2b9
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-09-07 11:21:41 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Justin Hold on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
